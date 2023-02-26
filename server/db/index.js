220acc1ae9f9bb110ba0dd4a418363aeaa2356b6
Mohammed <sulemanshah432@gmail.com>
2023-02-26 05:15:51 +0000

    backend for review

---
+++ server/db/index.js
@@ -0,0 +1,19 @@
+const dotenv = require("dotenv");
+const mongoose = require('mongoose');
+
+dotenv.config();
+
+mongoose.connect(
+  process.env.MONGO_URL,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => {
+    console.log("Connected to MongoDB");
+  },
+  (err) => {
+    console.error("Connection error", err.message);
+  }
+);
+
+const db = mongoose.connection
+
+module.exports = db
---
+++ server/package.md
@@ -0,0 +1,6 @@
+cd server
+
+npm install nodemon express mongoose morgan cors dotenv
+
+run = "npm run --prefix client start"
+entrypoint = "client/src/App.jsx"
