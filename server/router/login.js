e296eef9c7098dc12c31d4367d4d5de8d6579e13
Ayush Kumar <ayushdotdev@gmail.com>
2023-02-26 13:03:59 +0530

    done with login.js file of router/login.js

--- server/router/login.js
+++ server/router/login.js
@@ -1,13 +1,16 @@
 const express = require("express")
 const userModel=require("../model/User");
 const auth = require("../controller/auth");
-const Story=require("../controller/storyPost");
+const Story=require("../controller/story");
 const { authenticateToken } = require("../middlewares/authenticateToken");
 const Router=express.Router();
 const rbacMiddleware=require("../middlewares/accessControl")
 
 Router.post("/login",auth.login);
 Router.post("/register",auth.register);
-Router.get('/protected', authenticateToken,Story);
+Router.post('/protected', authenticateToken,Story.createOne);
+Router.get('/protected', authenticateToken,Story.findAll);
+Router.get('/protected/:id', authenticateToken,Story.findOneById);
+
 
 module.exports=Router 
